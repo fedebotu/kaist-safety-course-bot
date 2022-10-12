80d497573e27c07905255da18d1573dd97ae87f1
Federico Berto <berto.federico2@gmail.com>
2022-10-13 02:09:30 +0900

    Add new login options

--- kaist/navigation/login.py
+++ kaist/navigation/login.py
@@ -20,15 +20,19 @@ def portal_login(driver, config, timeout=60):
                 button = driver.find_element(by='xpath', value='//input[@type="submit" and @value="간편 인증"]')
                 button.click()
                 print('Authenticate login with your phone. Alternatively, you can use the opened browser to navigate.')
-            except: pass
-            wait = WebDriverWait(driver, timeout=timeout, poll_frequency=1)
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//*[ contains (text(), '간편인증이 진행중입니다')]")))
-            print("Waiting for login... Will timeout after `timeout` seconds")
-            wait.until_not(EC.element_to_be_clickable((By.XPATH, "//*[ contains (text(), '간편인증이 진행중입니다')]")))
-        else:
-            print('Manual login')
-            print('Please login manually. You can use the opened browser to navigate the page.')
-            WebDriverWait(driver, timeout=timeout, poll_frequency=1).until(EC.element_to_be_clickable((By.ID, 'IdInput')))
-            print('Login complete')
-            return
+                wait = WebDriverWait(driver, timeout=timeout, poll_frequency=1)
+                wait.until(EC.element_to_be_clickable((By.XPATH, "//*[ contains (text(), '간편인증이 진행중입니다')]")))
+                print("Waiting for login... Will timeout after `timeout` seconds")
+                wait.until_not(EC.element_to_be_clickable((By.XPATH, "//*[ contains (text(), '간편인증이 진행중입니다')]")))
+                return
+            except: 
+                print('Automatic login failed. Trying manual login...')
+                pass
+        
+        # Default behavior
+        print('Manual login')
+        print('Please login manually. You can use the opened browser to navigate the page.')
+        WebDriverWait(driver, timeout=timeout, poll_frequency=1).until(EC.element_to_be_clickable((By.ID, 'IdInput')))
+        # print('Login complete')
+        return
     
